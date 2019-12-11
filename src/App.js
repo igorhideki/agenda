b603dbf6028f1010db9dc0713e40384ba9b11d86
Igor Trindade <igorhideki@gmail.com>
2019-12-11 06:11:54 -0400

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyles from '~/styles/global';
+
 function App() {
-  return <h1>Agenda</h1>;
+  return (
+    <>
+      <h1>Agenda</h1>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/variables.js
@@ -0,0 +1,16 @@
+export const colors = {
+  primary: '#fa7268',
+  accent: '#dbff90',
+  lightGray: '#e4e7f4',
+  gray: '#9198af',
+  darkGray: '#2a2d3b',
+  borderGray: '#e1e1e1',
+  bgGray: '#f8f9fd',
+  pinkLight: '#fff3f2',
+};
+
+export const breakingPoints = {
+  md: '1024px',
+  sm: '839px',
+  xs: '575px',
+};
