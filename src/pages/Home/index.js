fb23c00224dac8361684b964af4cbcf3ca9f3b54
Igor Trindade <igorhideki@gmail.com>
2019-12-14 18:04:46 -0400

    Add home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Header from '~/components/Header';
-import { Container } from './styles';
+import DataTable from '~/components/DataTable';
+import EmptyListMessage from '~/components/EmptyListMessage';
+import ContactModal from '~/components/ContactModal';
+import { Container, Content } from './styles';
 
 export default function Home() {
+  const contacts = useSelector(state => state.schedule.contacts);
+  const headers = ['Contatos', 'E-mail', 'Telefone'];
+
   return (
     <Container>
       <Header />
-      <h1>Home</h1>
+      <ContactModal />
+      {contacts.length ? (
+        <Content>
+          <DataTable headers={headers} data={contacts} />
+        </Content>
+      ) : (
+        <EmptyListMessage />
+      )}
     </Container>
   );
 }
