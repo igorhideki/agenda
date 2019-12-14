67099e353c22e0aa82ccc864245759c39bdcd06e
Igor Trindade <igorhideki@gmail.com>
2019-12-14 14:52:16 -0400

    Add action to show modal

--- src/store/modules/schedule/actions.js
+++ src/store/modules/schedule/actions.js
@@ -1,6 +1,13 @@
-export function addNewContact(name, email, phone) {
+export function addNewContact({ name, email, phone }) {
   return {
     type: '@schedule/ADD_NEW_CONTACT',
     payload: { name, email, phone },
+  };
+}
+
+export function showContactModal(flag) {
+  return {
+    type: '@schedule/SHOW_CONTACT_MODAL',
+    payload: flag,
   };
 }
--- src/store/modules/schedule/reducer.js
+++ src/store/modules/schedule/reducer.js
@@ -1,9 +1,15 @@
 const INITIAL_STATE = {
   contacts: [],
+  isOpenContactModal: false,
+  isOpenDeleteModal: false,
 };
 
 export default function schedule(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case '@schedule/ADD_NEW_CONTACT':
+      return { ...state, contacts: [...state.contacts, action.payload] };
+    case '@schedule/SHOW_CONTACT_MODAL':
+      return { ...state, isOpenContactModal: action.payload };
     default:
       return state;
   }
